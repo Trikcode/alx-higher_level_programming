160c487b0509120eb119fc679aafdd16fea22797
Nobert Patrick Wise <ayesiganobert@gmail.com>
2021-10-11 21:39:17 +0300

    Inheritance🕊🕊🕊

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""defining the function lookup"""
+
+
+def lookup(obj):
+    """returns list object"""
+    return dir(obj)
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""creating BaseGeometry class"""
+
+
+class BaseGeometry:
+    """empty class"""
+    pass
