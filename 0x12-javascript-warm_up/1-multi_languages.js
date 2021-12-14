aa7bfb1011f95104f3a45678998f154ada611f0f
Nobert Patrick Wise <ayesiganobert@gmail.com>
2021-12-14 09:18:31 +0300

    JS

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,4 +1,8 @@
 #!/usr/bin/node
-console.log("C is fun");
-console.log("Python is cool");
-console.log("JavaScript is amazing");
+
+/**
+ * a script that prints three lines
+ */
+console.log('C is fun');
+console.log('Python is cool');
+console.log('JavaScript is amazing');
