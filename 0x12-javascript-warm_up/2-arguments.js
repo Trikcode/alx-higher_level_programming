05d6627b4761c79ee45f0b65b577209fd88ccc4d
Nobert Patrick Wise <ayesiganobert@gmail.com>
2021-12-14 09:22:43 +0300

    js

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,3 @@
 #!/usr/bin/node
-if (process.argv.length === 2) {
-  console.log('No argument');
-} else if (process.argv.length === 3) {
-  console.log('Argument found');
-} else if (process.argv.length > 3) {
-  console.log('Arguments found');
-}
+const count = process.argv.length;
+console.log(count === 2 ? 'No argument' : count === 3 ? 'Argument found' : 'Arguments found');
