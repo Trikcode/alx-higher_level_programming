c3c8d1b828d0a7d724e4f1c15906874fdcec7425
Wise2525 <ayesiganobert@gmail.com>
2022-01-09 12:31:28 -0800

    yes we can

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
-"""Fetches https://intranet.hbtn.io/status."""
+'''Python script that fetches https://intranet.hbtn.io/status'''
 import urllib.request
 
-
 if __name__ == "__main__":
-    request = urllib.request.Request("https://intranet.hbtn.io/status")
-    with urllib.request.urlopen(request) as response:
-        body = response.read()
+    url = 'https://intranet.hbtn.io/status'
+
+    with urllib.request.urlopen(url) as response:
+        html = response.read()
+
         print("Body response:")
-        print("\t- type: {}".format(type(body)))
-        print("\t- content: {}".format(body))
-        print("\t- utf8 content: {}".format(body.decode("utf-8")))
+        print("\t- type: {}".format(type(html)))
+        print("\t- content: {}".format(html))
+        print("\t- utf8 content: {}".format(html.decode(encoding='UTF-8')))
