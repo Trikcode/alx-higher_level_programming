3d1e5aa317f6b90309a86ea0bf5385a1a5c15167
Wise2525 <ayesiganobert@gmail.com>
2022-01-09 12:45:28 -0800

    never give up

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == "__main__":
     with urllib.request.urlopen(url) as response:
         html = response.read()
         print("Body response:")
-        print("\t- type:", type(html))
-        print("\t- content:", html)
-        print("\t- utf8 content:", html.decode("utf-8"))
+        print("-\t type:", type(html))
+        print("-\t content:", html)
+        print("-\t utf8 content:", html.decode("utf-8"))
