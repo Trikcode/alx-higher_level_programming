fa02e6b70ba168c0bf3664e9734074f7f67fcdac
Wise2525 <ayesiganobert@gmail.com>
2022-02-01 14:46:22 -0800

    missed a comma

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,11 +1,10 @@
 #!/usr/bin/node
 const fs = require('fs');
-const file = process.argv[2];
-fs.readFile(file, 'utf-8', function (err, data) {
+
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log(data.toString());
   }
 });
-
